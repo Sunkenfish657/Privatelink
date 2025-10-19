fee23506057edd063acf1fdd058f29e23cab60e0
Diana <nadiamegautami9@gmail.com>
2025-10-19 16:47:38 +0700

    main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+function showStegoPrototype() {
+  document.getElementById("main-content").classList.add("hidden");
+  document.getElementById("stego-prototype").classList.remove("hidden");
+}
+
+function hideStegoPrototype() {
+  document.getElementById("stego-prototype").classList.add("hidden");
+  document.getElementById("main-content").classList.remove("hidden");
+}
